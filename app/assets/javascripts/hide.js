8d36525d7af0665ee64517eebcc58c3645b8a035
Randall Harris Haynes <harrishaynes@gmail.com>
2018-06-05 20:25:56 +0000

    automatically backed up by learn

--- app/assets/javascripts/hide.js
+++ app/assets/javascripts/hide.js
@@ -1,4 +1,4 @@
-function hideWhenClicked(){
-  let el = document.getElementById('hide_this');
-  el.style.display = 'none';
-}
+const el = document.getElementById('hide_this');
+el.onclick = function(){
+  
+}.style.display = 'none';
